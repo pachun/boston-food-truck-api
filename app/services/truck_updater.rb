fced425af492a32b391bad0f3b95039232c094df
Nick Pachulski <nick@pachulski.me>
2015-08-03 15:02:42 -0400

    Refactor TruckUpdater.trucks to reduce complexity.

--- app/services/truck_updater.rb
+++ app/services/truck_updater.rb
@@ -1,24 +1,35 @@
 require "open-uri"
 
 class TruckUpdater
+  NUMBER_OF_HEADER_ROWS = 3
+
   def self.trucks
-    food_truck_html = Nokogiri::HTML(open("http://www.cityofboston.gov/foodtrucks/schedule-app-min.asp"))
-
-    food_truck_table = food_truck_html.css("#tableFoodTrucks")
-
     food_trucks = []
-    food_truck_table.xpath("//tr").each_with_index do |row, pos|
-      next unless pos > 3
-      cols = row.search("td")
-
-      food_trucks << {
-        name: cols[1].children.children.to_s,
-        day: cols[2].children.first.to_s,
-        time: cols[3].children.first.to_s,
-        location: cols[4].children.last.to_s,
-      }
+    food_truck_table_html.xpath("//tr").each_with_index do |row, pos|
+      next unless pos > NUMBER_OF_HEADER_ROWS
+      html_columns = row.search("td")
+      add_truck(food_trucks, html_columns)
     end
-
     food_trucks
+  end
+
+  private
+
+  def self.food_truck_page_html
+    food_truck_page = open("http://www.cityofboston.gov/foodtrucks/schedule-app-min.asp")
+    Nokogiri::HTML(food_truck_page)
+  end
+
+  def self.food_truck_table_html
+    food_truck_page_html.css("#tableFoodTrucks")
+  end
+
+  def self.add_truck(food_trucks, html_columns)
+    food_trucks << {
+      name: html_columns[1].children.children.to_s,
+      day: html_columns[2].children.first.to_s,
+      time: html_columns[3].children.first.to_s,
+      location: html_columns[4].children.last.to_s,
+    }
   end
 end
